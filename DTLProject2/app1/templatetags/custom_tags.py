4b6fa30364f7f11a639a52bffe541c88416bc683
Visweswararao <134912955+visweswararao3005@users.noreply.github.com>
2024-08-25 20:46:13 +0530

    Delete DTLProject2 directory

--- DTLProject2/app1/templatetags/custom_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def list_sum(value):
-    return sum(value)
--- DTLProject2/app1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from app1 import views
-
-urlpatterns = [
-    path('', views.home),
-]
