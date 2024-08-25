a65d0eb8024c4a3f3d316bea09c0b6209957b76b
Visweswararao <134912955+visweswararao3005@users.noreply.github.com>
2024-08-25 20:47:04 +0530

    Delete PracticeProject5 directory

--- PracticeProject5/marriage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from marriage import views
-
-urlpatterns = [
-    path('', views.home),
-    path('display', views.display),
-]
--- PracticeProject5/year/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from year import views
-
-urlpatterns = [
-    path('', views.home),
-    path('display', views.display),
-]
